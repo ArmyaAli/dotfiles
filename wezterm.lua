c9defc57a027debc01a65311420da701bf66fe95
Ali Umar <ali.umar.work@gmail.com>
2025-06-16 18:42:25 -0400

    add window splittling like neovim;

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,11 @@
 local wezterm = require 'wezterm'
+
 local config = {}
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+local act = wezterm.action
 
 config.color_scheme = 'Batman'
 config.automatically_reload_config = true
@@ -14,6 +20,35 @@ config.window_padding = {
   bottom = '0cell'
 }
 
-config.enable_tab_bar = false
+
+-- This table defines the keys that will be active *after* you press Ctrl-w.
+-- It's like Neovim's window command mode.
+config.key_tables = {
+  nvim_like_mode = {
+    { key = 's', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = 'v', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = 'h', action = act.ActivatePaneDirection 'Left' },
+    { key = 'j', action = act.ActivatePaneDirection 'Down' },
+    { key = 'k', action = act.ActivatePaneDirection 'Up' },
+    { key = 'l', action = act.ActivatePaneDirection 'Right' },
+  },
+}
+
+-- This is the main keybinding configuration.
+config.keys = {
+  {
+    key = 'w',
+    mods = 'CTRL',
+    -- This is the crucial part.
+    -- We check the name of the program running in the pane.
+    -- The '~' means NOT. So, this keybinding is active only when the
+    -- process name is NOT nvim (or vi, or vim).
+    pane = {
+      foreground_process_name = '~nvim|vim|vi',
+    },
+    -- When pressed, it activates our key table for 1 second.
+    action = act.ActivateKeyTable { name = 'nvim_like_mode', timeout_milliseconds = 1000 },
+  },
+}
 
 return config
