2facf36a902f50d98495a1073ff359e391ff2390
Ali Umar <ali.umar.work@gmail.com>
2025-06-16 21:29:58 -0400

    update config

--- wezterm.lua
+++ wezterm.lua
@@ -1,13 +1,9 @@
 local wezterm = require 'wezterm'
 
 local config = {}
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
 
 local act = wezterm.action
 
-config.color_scheme = 'Batman'
 config.automatically_reload_config = true
 config.color_scheme = 'Gruvbox dark, hard (base16)'
 config.default_prog = { 'powershell' }
@@ -20,13 +16,12 @@ config.window_padding = {
   bottom = '0cell'
 }
 
-
 -- This table defines the keys that will be active *after* you press Ctrl-w.
 -- It's like Neovim's window command mode.
 config.key_tables = {
   nvim_like_mode = {
-    { key = 's', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-    { key = 'v', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = 'v', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = 's', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
     { key = 'h', action = act.ActivatePaneDirection 'Left' },
     { key = 'j', action = act.ActivatePaneDirection 'Down' },
     { key = 'k', action = act.ActivatePaneDirection 'Up' },
@@ -34,20 +29,15 @@ config.key_tables = {
   },
 }
 
--- This is the main keybinding configuration.
 config.keys = {
   {
     key = 'w',
-    mods = 'CTRL',
-    -- This is the crucial part.
-    -- We check the name of the program running in the pane.
-    -- The '~' means NOT. So, this keybinding is active only when the
-    -- process name is NOT nvim (or vi, or vim).
-    pane = {
-      foreground_process_name = '~(?i)nvim\\.exe',
-    },
-    -- When pressed, it activates our key table for 1 second.
-    action = act.ActivateKeyTable { name = 'nvim_like_mode', timeout_milliseconds = 1000 },
+    mods = 'SHIFT',
+    action = wezterm.action_callback(function(window, pane)
+        window:perform_action( act.ActivateKeyTable { name = 'nvim_like_mode', timeout_milliseconds = 1000 },
+          pane
+        )
+    end),
   },
 }
 
